acd953ce8fec842f50dbf22b5157af48393d36d8
Ahmet Candan <54511663+ahmet-candan@users.noreply.github.com>
2020-09-21 13:24:41 +0300

    Day-9

---
+++ karakter_dizileri.py
@@ -0,0 +1,9 @@
+kardiz = "istihza"
+
+for i in range(len(kardiz)):
+    print(kardiz[i])
+
+isim = input("isminiz: ")
+
+for i in range(len(isim)):
+    print("isminizin {}. harfi: {}".format(i+1,isim[i]))
