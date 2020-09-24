221e32d5ee465a7db93e813db89561e136f8331d
Ahmet Candan <54511663+ahmet-candan@users.noreply.github.com>
2020-09-24 23:51:34 +0300

    Update karakter_dizileri.py

--- karakter_dizileri.py
+++ karakter_dizileri.py
@@ -7,3 +7,20 @@ isim = input("isminiz: ")
 
 for i in range(len(isim)):
     print("isminizin {}. harfi: {}".format(i+1,isim[i]))
+    
+    
+site1 = "www.google.com"
+site2 = "www.istihza.com"
+site3 = "www.yahoo.com"
+site4 = "www.gnu.org"
+for isim in site1, site2, site3, site4:
+    print("site: ", isim[4:-4])
+    """
+    Bu örnek Python’da dilimleme islemlerinin yapısı ve özellikleri hakkında bize epey bilgi
+veriyor. Gördügünüz gibi, hem artı hem de eksi degerli sayıları kullanabiliyoruz. Önceki
+bölümden hatırlayacagınız gibi, eger verilen sayı eksi degerliyse Python karakter dizisini
+sagdan sola (yani sondan basa dogru) okuyacaktır. Yukarıdaki örnekte isim[4:-4] yapısını
+kullanarak, site1, site2, site3, site4 adlı karakter dizilerini, ilk dört ve son dört karakterler
+hariç olacak sekilde dilimledik. Böylece elimizde ilk dört ve son dört karakter arasındaki bütün
+karakterler kalmıs oldu. Yani “google”, “istihza”, “yahoo” ve “gnu”.
+"""
